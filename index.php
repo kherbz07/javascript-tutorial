71ccc980624e369a84fdb92e9d779669e486159e
Kirby Lopez <kherbz07@gmail.com>
2017-06-21 15:47:21 +0800

    Created html template for index.

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-echo 'Hello World';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Javascript Tutorial</title>
+</head>
+<body>
+	<h3>Hello World!</h3>
+</body>
+</html>
